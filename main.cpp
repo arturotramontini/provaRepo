3a48fdfc5acefb3cab8c026d8ca0c893acdf18e7
arturo tramontini <arturo.tramontini@gmail.com>
2019-10-05 18:55:10 +0200

    questo thread funziona !

--- main.cpp
+++ main.cpp
@@ -1,14 +1,37 @@
 #include <stdio.h>
-#include <thread>
 
+// preso da qui: https://www.includehelp.com/articles/threading-in-c-programming-language-with-gcc-linux.aspx
 
-
-int main(){
-  int c;
-  puts ("Enter text. Include a dot ('.') in a sentence to exit:");
-  do {
-    c=getchar();
-    putchar (c);
-  } while (c != '.');
-  return 0;
+#include <pthread.h>
+        
+/*thread function definition*/
+void* threadFunction(void* args)
+{
+    while(1)
+    {
+        printf("I am threadFunction.\n");
+    }
+}
+int main()
+{
+    /*creating thread id*/
+    pthread_t id;
+    int ret;
+    
+    /*creating thread*/
+    ret=pthread_create(&id,NULL,&threadFunction,NULL);
+    if(ret==0){
+        printf("Thread created successfully.\n");
+    }
+    else{
+        printf("Thread not created.\n");
+        return 0; /*return from main*/
+    }
+    
+    while(1)
+    {
+        printf("I am main function.\n");      
+    }
+    
+    return 0;
 }
